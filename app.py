df50294c3b1c23e950a86ee435ec873314fb2a23
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-04 01:47:38 +0300

    m

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ with st.sidebar:
                          menu_icon="app-indicator", 
                          default_index=0,
                          styles={
-                                "container": {"padding": "5!important", "background-color": "#fafafa"},
-                                "icon": {"color": "orange", "font-size": "25px"}, 
-                                "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-                                "nav-link-selected": {"background-color": "#02ab21"},
+                                "container": {"padding": "5!important", },#"background-color": "#fafafa"
+                                "icon": {"font-size": "25px"}, #"color": "orange"
+                                "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px" },#"--hover-color": "#eee"
+                               #"nav-link-selected": {"background-color": "#02ab21"},
                                 }
                         )
 
