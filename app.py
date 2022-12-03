fa92be97cdfa34182f77a22c0801646d9c2e22f1
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-04 02:09:22 +0300

    m

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit.components.v1 as html
 
 
 with st.sidebar:
-    choose = option_menu("Hacettepe Büyüme & Gelişim", ["Z skoru", "Gelişim Basamakları"],
+    choose = option_menu("Hacettepe Büyüme & Gelişim", ["Z skoru", "Gelişim Basamakları", "Uygulama geliştirici"],
                          icons=['bi bi-calculator-fill', 'bi bi-arrows-fullscreen'],
                          # icons = https://icons.getbootstrap.com/
                          menu_icon="app-indicator", 
@@ -30,83 +30,79 @@ with st.sidebar:
 
 if choose == "Z skoru":
     col1, col2, col3 = st.columns( [0.1, 0.8, 0.1])
-    with col1:
+    with col2:               # To display the header text using css style   with col2:    
+    DETA_KEY = "a06jeh1v_GJmS8DEiFToMLujbKenNi4rKPKj4fMNr"
 
-        st.write("geliyor") 
+    deta = Deta(DETA_KEY)
+    users = deta.Base("bmi")
 
-    with col3:
 
-        st.write("geliyor") 
 
-    with col2:       
+    st.title("Z-score Hesaplama Uygulaması")
+
+    img = Image.open("./images/hacettepe.jpg")
+    st.image(img)
+
+    # Introduction
+
+    st.subheader("Büyüme Takibi")
+
+
+    st.text("""
+    Bu uygulamada Z score'lar 2 yaş ve altında WHO üstünde ise \n
+    CDC referans aralıkları kullanılarak hesaplanır.\n
+    Hizmet kalitesinde artış amaçlı olarak girdiğiniz veriler kayıt altına alınacaktır.
+        """)
+
+
+    weight = st.number_input("KG cinsinden ağırlık;", step = 1)
+    height = st.number_input("Santimetre cinsinden boyu;", step = 1)
+    age = st.number_input("Ay olarak yaşı;",step=1)
+    gender =st.radio("Cinsiyeti seçiniz", options=["Erkek","Kız"])
+        #text_input("Erkek için E Kız için K yazınız;")
+
+    if gender == "Erkek":
+        gender = "M"
+    if gender == "Kız":
+        gender = "F"
+
+    if st.button("Z skorunu Analiz Et"):
+
+
+
+        wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
+        wfa = float(str(wfa))
+        lhfa = float(str(lhfa))
         
-        st.markdown(""" <style> .font {
-        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-        </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="font"> Z skoru hesaplayıcısı </p>', unsafe_allow_html=True) 
-
-        DETA_KEY = "a06jeh1v_GJmS8DEiFToMLujbKenNi4rKPKj4fMNr"
-
-        deta = Deta(DETA_KEY)
-        users = deta.Base("bmi")
-
-
-
-        st.title("Z-score Hesaplama Uygulaması")
-
-        img = Image.open("./images/hacettepe.jpg")
-        st.image(img)
-
-        # Introduction
-
-        st.subheader("Büyüme Takibi")
-
-
-        st.text("""
-        Bu uygulamada Z score'lar 2 yaş ve altında WHO üstünde ise \n
-        CDC referans aralıkları kullanılarak hesaplanır.\n
-        Hizmet kalitesinde artış amaçlı olarak girdiğiniz veriler kayıt altına alınacaktır.
-            """)
-
-
-        weight = st.number_input("KG cinsinden ağırlık;", step = 1)
-        height = st.number_input("Santimetre cinsinden boyu;", step = 1)
-        age = st.number_input("Ay olarak yaşı;",step=1)
-        gender =st.radio("Cinsiyeti seçiniz", options=["Erkek","Kız"])
-            #text_input("Erkek için E Kız için K yazınız;")
-
-        if gender == "Erkek":
-            gender = "M"
-        if gender == "Kız":
-            gender = "F"
-
-        if st.button("Z skorunu Analiz Et"):
-
-
-
-            wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
-            wfa = float(str(wfa))
-            lhfa = float(str(lhfa))
-            
-            st.success(f"Yaşa göre ağırlık; {wfa}") 
-            st.success(f"Yaşa göre boy; {lhfa}") #st.success(f"Sonuç: {result}")
-            
-            response = {
-                            'age': age,
-                            'weight': weight,
-                            'height': height,
-                            'gender': gender,
-                            'wfa':wfa,
-                            'lhfa':lhfa
-                        }
-            users.insert(response)
-            st.text("Bilgiler Database'e aktarıldı")
+        st.success(f"Yaşa göre ağırlık; {wfa}") 
+        st.success(f"Yaşa göre boy; {lhfa}") #st.success(f"Sonuç: {result}")
+        
+        response = {
+                        'age': age,
+                        'weight': weight,
+                        'height': height,
+                        'gender': gender,
+                        'wfa':wfa,
+                        'lhfa':lhfa
+                    }
+        users.insert(response)
+        st.text("Bilgiler Database'e aktarıldı")
 
 if choose == "Gelişim Basamakları":
 
     st.write("Hazırlanıyor",size=32)
     img = Image.open("./images/developmental_steps.jpg")
     st.image(img)
+
+if choose == "Uygulama geliştirici":
+
+    
+    profile = Image.open(r'./images/turkalpmd.jpg')
+    
+    st.write("Çalışmalarım; [link](https://turkalpmd.github.io)")
+
+    st.image(profile, width=700 )
+
 
 
 
