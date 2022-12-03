692ffc2a17d850ac8efbca3eca4e9306980ae9dc
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-04 01:46:15 +0300

    m

--- app.py
+++ app.py
@@ -39,13 +39,6 @@ if choose == "Z skoru":
         users = deta.Base("bmi")
 
 
-        with st.sidebar:
-            with st.echo():
-                st.write("This code will be printed to the sidebar.")
-
-            with st.spinner("Loading..."):
-                time.sleep(5)
-            st.success("Done!")
 
         st.title("Z-score Hesaplama Uygulaması")
 
