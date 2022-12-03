602e18246530806e30f38641c61600e52adfe409
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-04 01:02:10 +0300

    m

--- app.py
+++ app.py
@@ -1,42 +1,70 @@
+
+from PIL import Image
 import streamlit as st
+from utils import *
+import pandas as pd
+import numpy as np
 from deta import Deta
 
 DETA_KEY = "a06jeh1v_GJmS8DEiFToMLujbKenNi4rKPKj4fMNr"
 
 deta = Deta(DETA_KEY)
-
 users = deta.Base("bmi")
 
-# Input fields
-age = st.number_input('Age:')
-weight = st.number_input('Weight (in kg):')
-height = st.number_input('Height (in cm):')
-gender = st.radio('Gender:', ['Male', 'Female'])
+st.title("Z-score Hesaplama Uygulaması")
 
-# BMI calculation function
-def calc_bmi(weight, height):
-    result = (weight / (height / 100) ** 2)
-    return result
+img = Image.open("hacettepe.jpg")
+st.image(img)
+
+# Introduction
+
+st.subheader("Büyüme Takibi")
 
 
-
-if st.button('Submit'):
-
-    if weight > 0 and height > 0:
-
-        bmi = calc_bmi(weight, height)
-
-        response = {
-            'age': age,
-            'weight': weight,
-            'height': height,
-            'bmi': bmi,
-        }
-        users.insert(response)
-        st.write('Data saved to the database')
-    else:
-        st.write("Can you input relevant informations")
+st.text("""
+Bu uygulamada Z score'lar 2 yaş ve altında WHO üstünde ise \n
+CDC referans aralıkları kullanılarak hesaplanır.
+	""")
 
 
+weight = st.number_input("KG cinsinden ağırlık;", step = 0.1)
+height = st.number_input("Santimetre cinsinden boyu;")
+age = st.number_input("Ay olarak yaşı;",step=1)
+gender =st.radio("Cinsiyeti seçiniz", options=["Erkek","Kız"])
+    #text_input("Erkek için E Kız için K yazınız;")
+
+if gender == "Erkek":
+    gender = "M"
+if gender == "Kız":
+    gender = "F"
+
+df = pd.DataFrame()
+df["weight"] = weight
+df["height"] = height
+df["age"] = age
+df["gender"] = gender
+
+uploaded_files = df
+
+
+if st.button("Z skorunu Analiz Et"):
+
+
+
+    wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
+    df["wfa"] = wfa
+    df["lhfa"] = lhfa
     
-
+    st.success(f"Yaşa göre ağırlık; {wfa}") 
+    st.success(f"Yaşa göre boy; {lhfa}") #st.success(f"Sonuç: {result}")
+    
+    response = {
+                    'age': age,
+                    'weight': weight,
+                    'height': height,
+                    'gender': gender,
+                    'wfa':wfa,
+                    'lhfa':lhfa
+                }
+    users.insert(response)
+    st.write('Data saved to the database')
